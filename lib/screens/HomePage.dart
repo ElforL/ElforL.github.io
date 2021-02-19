f5b76bf27ef8348c7a2b5d80e6c589f00be6b193
Elfor <lshono8@gmail.com>
2021-02-19 15:15:48 +0300

    ✨ Added link to GitHub page

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -3,13 +3,23 @@ import 'package:portfolio/screens/HomePages/About.dart';
 import 'package:portfolio/screens/HomePages/Contact.dart';
 import 'package:portfolio/screens/HomePages/Projects.dart';
 import 'package:portfolio/screens/HomePages/Tools.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MyHomePage extends StatelessWidget {
   final projectsKey = GlobalKey();
   final toolsKey = GlobalKey();
   final contactKey = GlobalKey();
+  static const githubPageURL = 'https://github.com/ElforL/ElforL.github.io';
 
   MyHomePage({Key key}) : super(key: key);
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,6 +57,33 @@ class MyHomePage extends StatelessWidget {
                 padding: const EdgeInsets.symmetric(vertical: 100),
                 child: ContactPage(),
               ),
+              Padding(
+                padding: const EdgeInsets.only(top: 100, bottom: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'This website is an open source project ',
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                    TextButton(
+                      onPressed: () => _launchURL(githubPageURL),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.transparent),
+                        overlayColor: MaterialStateProperty.all(Colors.transparent),
+                        shadowColor: MaterialStateProperty.all(Colors.transparent),
+                      ),
+                      child: Text(
+                        'available on my GitHub.',
+                        style: Theme.of(context).textTheme.caption.copyWith(
+                              color: Colors.blue,
+                              decoration: TextDecoration.underline,
+                            ),
+                      ),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
