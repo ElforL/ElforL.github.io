bb082b378ee3c280c0e22d42228df3437f1c4443
Elfor <elfordev@gmail.com>
2021-06-22 13:55:29 +0300

    🖌 made all the projects show together not split into small and full anymore also fixes #1

--- lib/screens/HomePages/Projects.dart
+++ lib/screens/HomePages/Projects.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:portfolio/models/Project.dart';
 import 'package:portfolio/widgets/ProjectTile.dart';
@@ -24,53 +22,15 @@ class Projects extends StatelessWidget {
           constraints: BoxConstraints(
             maxWidth: 1200,
           ),
-          child: FutureBuilder(
-            future: loadProjects(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState != ConnectionState.done) return Center(child: CircularProgressIndicator());
-              projects = snapshot.data;
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 40),
-                    child: Text(
-                      'Full apps',
-                      style: Theme.of(context).textTheme.headline4,
-                    ),
-                  ),
-                  Wrap(
-                    spacing: 10,
-                    runSpacing: 10,
-                    children: [
-                      for (var project in projects)
-                        if (!project.isSmall)
-                          ProjectTile(
-                            project: project,
-                          ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 40),
-                    child: Text(
-                      'Small fun projects',
-                      style: Theme.of(context).textTheme.headline4,
-                    ),
-                  ),
-                  Wrap(
-                    spacing: 10,
-                    runSpacing: 10,
-                    children: [
-                      for (var project in projects)
-                        if (project.isSmall)
-                          ProjectTile(
-                            project: project,
-                          ),
-                    ],
-                  ),
-                ],
-              );
-            },
+          child: Wrap(
+            spacing: 10,
+            runSpacing: 10,
+            children: [
+              for (var project in projects)
+                ProjectTile(
+                  project: project,
+                ),
+            ],
           ),
         ),
       ],
