ef68b6883c213848c97727b782b6d2edfd29a44d
Elfor <elfordev@gmail.com>
2021-06-22 04:38:15 +0300

    🖌 HomePage migrate

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -4,7 +4,6 @@ import 'package:portfolio/screens/HomePages/About.dart';
 import 'package:portfolio/screens/HomePages/Contact.dart';
 import 'package:portfolio/screens/HomePages/Projects.dart';
 import 'package:portfolio/screens/HomePages/Tools.dart';
-import 'package:url_launcher/link.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -26,51 +25,49 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              About(
-                contactPress: (duration) => Scrollable.ensureVisible(
-                  contactKey.currentContext,
-                  duration: duration,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            About(
+                // contactPress: (duration) => Scrollable.ensureVisible(
+                //   contactKey.currentContext,
+                //   duration: duration,
+                // ),
+                // projectsPress: (duration) => Scrollable.ensureVisible(
+                //   projectsKey.currentContext,
+                //   duration: duration,
+                // ),
+                // toolsPress: (duration) => Scrollable.ensureVisible(
+                //   toolsKey.currentContext,
+                //   duration: duration,
+                // ),
                 ),
-                projectsPress: (duration) => Scrollable.ensureVisible(
-                  projectsKey.currentContext,
-                  duration: duration,
-                ),
-                toolsPress: (duration) => Scrollable.ensureVisible(
-                  toolsKey.currentContext,
-                  duration: duration,
-                ),
-              ),
-              Padding(
-                key: projectsKey,
-                padding: const EdgeInsets.symmetric(vertical: 100),
-                child: Projects(),
-              ),
-              Padding(
-                key: toolsKey,
-                padding: const EdgeInsets.symmetric(vertical: 100),
-                child: Tools(),
-              ),
-              Padding(
-                key: contactKey,
-                padding: const EdgeInsets.symmetric(vertical: 100),
-                child: ContactPage(),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 100, bottom: 10),
-                child: Text.rich(
-                  TextSpan(
-                    text: 'Website source code available on GitHub',
+            Padding(
+              key: projectsKey,
+              padding: const EdgeInsets.symmetric(vertical: 100),
+              child: Projects(),
+            ),
+            Padding(
+              key: toolsKey,
+              padding: const EdgeInsets.symmetric(vertical: 100),
+              child: Tools(),
+            ),
+            Padding(
+              key: contactKey,
+              padding: const EdgeInsets.symmetric(vertical: 100),
+              child: ContactPage(),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 100, bottom: 10),
+              child: Text.rich(
+                TextSpan(
+                  text: 'Website source code available on GitHub',
                   style: Theme.of(context).textTheme.caption.copyWith(color: Colors.grey.shade800),
-                    recognizer: TapGestureRecognizer()..onTap = () => _launchURL(githubPageURL),
-                  ),
+                  recognizer: TapGestureRecognizer()..onTap = () => _launchURL(githubPageURL),
                 ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
       ),
     );
