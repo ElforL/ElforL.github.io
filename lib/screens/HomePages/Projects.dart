8f383e5f25c3de53f7b0234e1e215b98d3a6aa6e
Elfor <elfordev@gmail.com>
2021-06-25 13:10:04 +0300

    ✨ added 'more on GitHub' button.

--- lib/screens/HomePages/Projects.dart
+++ lib/screens/HomePages/Projects.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/main.dart';
 import 'package:portfolio/models/Project.dart';
 import 'package:portfolio/widgets/ProjectTile.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Projects extends StatelessWidget {
   final List<Project> projects;
 
   const Projects({Key key, @required this.projects}) : super(key: key);
 
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Padding(
           padding: const EdgeInsets.only(bottom: 40),
@@ -34,6 +45,16 @@ class Projects extends StatelessWidget {
             ],
           ),
         ),
+        TextButton.icon(
+          label: Text('More on my GitHub'),
+          icon: Icon(Icons.arrow_right_rounded),
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all(Colors.grey),
+          ),
+          onPressed: () {
+            _launchURL(dbServices.gitHubURL);
+          },
+        )
       ],
     );
   }
