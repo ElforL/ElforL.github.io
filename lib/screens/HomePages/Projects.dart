ea62e85be2e4850d0d0f8cc9268a40d775435c90
Laith Shono <elfordev@gmail.com>
2022-04-18 22:21:31 +0300

    ✨ Implement 3.0 design to `Projects`

--- lib/screens/HomePages/Projects.dart
+++ lib/screens/HomePages/Projects.dart
@@ -1,61 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:laith_shono/main.dart';
 import 'package:laith_shono/models/Project.dart';
+import 'package:laith_shono/services/misc.dart';
 import 'package:laith_shono/widgets/ProjectTile.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class Projects extends StatelessWidget {
   final List<Project>? projects;
 
   const Projects({Key? key, required this.projects}) : super(key: key);
 
-  _launchURL(String url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(bottom: 40),
-          child: Text(
-            'Projects',
-            style: Theme.of(context).textTheme.headline2,
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Container(
+      constraints: BoxConstraints(
+        minHeight: height,
+        minWidth: width,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 40),
+            child: Text(
+              AppLocalizations.of(context)!.selected_projects,
+              style: Theme.of(context).textTheme.headline2,
+            ),
           ),
-        ),
-        Container(
-          padding: EdgeInsets.all(10),
-          constraints: BoxConstraints(
-            maxWidth: 1200,
+          Container(
+            alignment: width > 510 ? null : Alignment.center,
+            child: Wrap(
+              spacing: 10,
+              runSpacing: 10,
+              children: [
+                for (var project in projects!)
+                  FittedBox(
+                    child: ProjectTile(
+                      project: project,
+                    ),
+                  ),
+              ],
+            ),
           ),
-          child: Wrap(
-            spacing: 10,
-            runSpacing: 10,
-            children: [
-              for (var project in projects!)
-                ProjectTile(
-                  project: project,
-                ),
-            ],
-          ),
-        ),
-        TextButton.icon(
-          label: Text('More on my GitHub'),
-          icon: Icon(Icons.arrow_right_rounded),
-          style: ButtonStyle(
-            foregroundColor: MaterialStateProperty.all(Colors.grey),
-          ),
-          onPressed: () {
-            _launchURL(dbServices.gitHubURL!);
-          },
-        )
-      ],
+          SizedBox(height: 20),
+          Container(
+            alignment: width > 510 ? null : AlignmentDirectional.center,
+            child: TextButton.icon(
+              label: Text(AppLocalizations.of(context)!.more_on_gh),
+              icon: Icon(Icons.arrow_right_rounded),
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.grey),
+              ),
+              onPressed: () {
+                launchURL(dbServices.gitHubURL!);
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
