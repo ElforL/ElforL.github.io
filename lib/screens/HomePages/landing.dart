70271c1253911e79f57bd6b1abfa92060820535d
Laith Shono <elfordev@gmail.com>
2022-04-18 16:06:12 +0300

    🎨 match text style in Landing to  3.0 design

--- lib/screens/HomePages/landing.dart
+++ lib/screens/HomePages/landing.dart
@@ -8,30 +8,34 @@ class Landing extends StatelessWidget {
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
+
     return Container(
       constraints: BoxConstraints(minHeight: height),
       width: width,
       child: Align(
         alignment: Alignment.centerLeft,
         child: Container(
-          constraints: BoxConstraints(maxWidth: 450),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
                 AppLocalizations.of(context)!.landing_hi,
-                style: Theme.of(context).textTheme.subtitle1,
+                style: Theme.of(context).textTheme.headline6,
               ),
+
+              // When the screen width is < 360 the name letters starts to wrap so a fitted box will keep
+              // the name as one line but shrinks it as the screen gets smaller
               FittedBox(
                 child: Text(
                   AppLocalizations.of(context)!.first_name,
-                  style: Theme.of(context).textTheme.headline1!.copyWith(fontWeight: FontWeight.w500),
+                  style: Theme.of(context).textTheme.headline1!.copyWith(color: Theme.of(context).colorScheme.primary),
                 ),
               ),
+
               Text(
                 AppLocalizations.of(context)!.landing_about,
-                style: Theme.of(context).textTheme.subtitle1,
+                style: Theme.of(context).textTheme.headline6,
               ),
             ],
           ),
