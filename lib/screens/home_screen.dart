29e26ce362324552945b4208a02b73a42f8857aa
Laith Shono <elfordev@gmail.com>
2022-04-19 18:51:57 +0300

    📝 Rename `MyHomePage` to `HomeScreen`

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,7 +9,7 @@ import 'package:laith_shono/services/misc.dart';
 import 'package:laith_shono/widgets/top_bar.dart';
 import 'package:laith_shono/widgets/trigger_fab.dart';
 
-class MyHomePage extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
   final fabKey = GlobalKey<TriggerFabState>();
   final projectsKey = GlobalKey();
   final skillsKey = GlobalKey();
@@ -19,7 +19,7 @@ class MyHomePage extends StatelessWidget {
 
   final scrollDuration = const Duration(milliseconds: 300);
 
-  MyHomePage({Key? key}) : super(key: key) {
+  HomeScreen({Key? key}) : super(key: key) {
     _scrollController.addListener(() {
       final fabShown = fabKey.currentState?.isShown ?? false;
 
