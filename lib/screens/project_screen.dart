d787f99b896d0787b97347f6cc08501bd090b839
Laith Shono <elfordev@gmail.com>
2022-04-22 05:27:32 +0300

    ✨ `ProjectScreen` can get the project using its title

--- lib/screens/project_screen.dart
+++ lib/screens/project_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:laith_shono/main.dart';
 import 'package:laith_shono/models/Project.dart';
 import 'package:laith_shono/models/project_block.dart';
+import 'package:laith_shono/screens/404.dart';
 import 'package:laith_shono/widgets/project_screen_blocks/banner_block.dart';
 import 'package:laith_shono/widgets/project_screen_blocks/full_width_image_block.dart';
 import 'package:laith_shono/widgets/project_screen_blocks/head_block.dart';
@@ -12,48 +14,74 @@ import 'package:laith_shono/widgets/socials_wrap.dart';
 class ProjectScreen extends StatelessWidget {
   static const routeName = '/project';
 
-  const ProjectScreen({
+  ProjectScreen({
     Key? key,
-    required this.project,
-  }) : super(key: key);
+    this.project,
+    String? projectTitle,
+  })  : projectName = projectTitle ?? (project?.title ?? ''),
+        super(key: key);
 
-  final Project project;
+  Project? project;
+  final String? projectName;
 
   @override
   Widget build(BuildContext context) {
-    final blocksList = project.screenBlocks;
+    assert(project != null || projectName != null);
 
-    List<ProjectBlock> blocks = [];
-    for (var blockJson in blocksList) {
-      final block = ProjectBlock.fromJson(blockJson);
-      blocks.add(block);
+    Future<Project?> future;
+    if (project == null) {
+      future = dbServices.getProject(projectName!);
+    } else {
+      future = Future.value(project);
     }
 
     return Scaffold(
-      // ListView is more efficient but since the children are of different sizes the scroll
-      // bar keeps changing in size as the user scrolls (due to widgets being constantly built/unbuilt)
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            for (var block in blocks) _buildBlockWidget(context, block),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
-              alignment: AlignmentDirectional.topEnd,
-              child: SizedBox(
-                height: 30,
-                child: FittedBox(
-                  child: SocialsWrap(),
-                ),
+      body: FutureBuilder(
+          future: future,
+          builder: (context, AsyncSnapshot<Project?> snapshot) {
+            if (snapshot.connectionState != ConnectionState.done) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            project = snapshot.data;
+
+            if (project == null) {
+              return PageNotFoundScreen();
+            }
+
+            final blocksList = project!.screenBlocks;
+            List<ProjectBlock> blocks = [];
+
+            for (var blockJson in blocksList) {
+              final block = ProjectBlock.fromJson(blockJson);
+              blocks.add(block);
+            }
+
+            // ListView is more efficient but since the children are of different sizes the scroll
+            // bar keeps changing in size as the user scrolls (due to widgets being constantly built/unbuilt)
+            return SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  for (var block in blocks) _buildBlockWidget(context, block),
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                    alignment: AlignmentDirectional.topEnd,
+                    child: SizedBox(
+                      height: 30,
+                      child: FittedBox(
+                        child: SocialsWrap(),
+                      ),
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
-      ),
+            );
+          }),
     );
   }
 
-  _buildBlockWidget(BuildContext context, ProjectBlock block) {
+  Widget _buildBlockWidget(BuildContext context, ProjectBlock block) {
     switch (block.type) {
       case ProjectBlockType.head:
         return HeadBlockWidget(block: block as HeadProjectBlock);
