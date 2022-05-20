8f1ee17d5fc8096ef5fb8c896068c70922fc8faa
Laith Shono <elfordev@gmail.com>
2022-05-20 14:54:01 +0300

    ✨ Add top bar to project screen

--- lib/screens/project_screen.dart
+++ lib/screens/project_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:laith_shono/main.dart';
 import 'package:laith_shono/models/Project.dart';
 import 'package:laith_shono/models/project_block.dart';
@@ -11,6 +14,7 @@ import 'package:laith_shono/widgets/project_screen_blocks/image_overhang_block.d
 import 'package:laith_shono/widgets/project_screen_blocks/image_with_padding_block.dart';
 import 'package:laith_shono/widgets/socials_wrap.dart';
 
+// ignore: must_be_immutable
 class ProjectScreen extends StatelessWidget {
   static const routeName = '/project';
 
@@ -59,24 +63,29 @@ class ProjectScreen extends StatelessWidget {
 
             // ListView is more efficient but since the children are of different sizes the scroll
             // bar keeps changing in size as the user scrolls (due to widgets being constantly built/unbuilt)
-            return SingleChildScrollView(
-              physics: BouncingScrollPhysics(),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  for (var block in blocks) _buildBlockWidget(context, block),
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
-                    alignment: AlignmentDirectional.topEnd,
-                    child: SizedBox(
-                      height: 30,
-                      child: FittedBox(
-                        child: SocialsWrap(),
-                      ),
-                    ),
-                  )
-                ],
-              ),
+            return Stack(
+              children: [
+                SingleChildScrollView(
+                  physics: BouncingScrollPhysics(),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      for (var block in blocks) _buildBlockWidget(context, block),
+                      Container(
+                        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                        alignment: AlignmentDirectional.topEnd,
+                        child: SizedBox(
+                          height: 30,
+                          child: FittedBox(
+                            child: SocialsWrap(),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                _buildTopBar(context),
+              ],
             );
           }),
     );
@@ -102,5 +111,70 @@ class ProjectScreen extends StatelessWidget {
       case ProjectBlockType.imageOverhang:
         return ImageOverhandBlockWidget(block: block as ImageOverhangProjectBlock);
     }
+  }
+
+  Widget _buildTopBar(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Theme.of(context).colorScheme.background,
+            Theme.of(context).colorScheme.background.withAlpha(178),
+            Theme.of(context).colorScheme.background.withAlpha(0),
+          ],
+          stops: [
+            0,
+            0.5,
+            1,
+          ],
+          begin: AlignmentDirectional.topCenter,
+          end: AlignmentDirectional.bottomCenter,
+        ),
+      ),
+      child: Row(
+        children: [
+          FittedBox(
+            child: Container(
+              margin: const EdgeInsets.all(15),
+              child: Text(
+                'Laith Shono',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.5,
+                ),
+              ),
+            ),
+          ),
+          Spacer(),
+          InkWell(
+            splashColor: Colors.transparent,
+            hoverColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Container(
+              margin: const EdgeInsets.all(15),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Transform(
+                    alignment: Alignment.center,
+                    transform: Matrix4.rotationY(pi),
+                    child: Icon(Icons.arrow_right_alt),
+                  ),
+                  SizedBox(width: 8),
+                  Text(
+                    AppLocalizations.of(context)!.back_home,
+                    style: Theme.of(context).textTheme.button,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
