b33d4396515f435198f6508f323721f92798bffd
Laith Shono <elfordev@gmail.com>
2022-06-24 19:23:10 +0300

    🚮 Remove Loading hero animation 🙂

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:laith_shono/widgets/web_emoji_loader.dart';
 
 class LoadingScreen extends StatelessWidget {
@@ -12,20 +11,7 @@ class LoadingScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Hero(
-              key: ValueKey('Name'),
-              tag: 'Name',
-              child: Text(
-                AppLocalizations.of(context)!.first_name,
-                style: Theme.of(context).textTheme.headline1!.copyWith(color: Theme.of(context).colorScheme.primary),
-              ),
-            ),
-            SizedBox(height: 50),
-            Container(
-              constraints: BoxConstraints(maxWidth: 400),
-              padding: const EdgeInsets.symmetric(horizontal: 80),
-              child: LinearProgressIndicator(color: Colors.white),
-            ),
+            CircularProgressIndicator(color: Colors.white),
             WebEmojiLoaderHack(),
           ],
         ),
