538939a23001b54a239297fc18846a58ac956b0b
Laith Shono <elfordev@gmail.com>
2022-06-26 19:49:24 +0300

    📝 Update `ElforConfiguration`

--- lib/router/elfor_configuration.dart
+++ lib/router/elfor_configuration.dart
@@ -1,23 +1,23 @@
 import 'package:laith_shono/models/Project.dart';
 
 class ElforConfiguration {
-  // final bool _show404;
-  // final bool? _loggedIn;
-  // final Project? _selectedProject;
+  final bool _show404;
+  final bool _initiated;
+  final Project? _selectedProject;
 
-  // ElforConfiguration(
-  //   this._show404,
-  //   this._loggedIn,
-  //   this._selectedProject,
-  // );
+  ElforConfiguration(
+    this._show404,
+    this._initiated,
+    this._selectedProject,
+  );
 
-  // factory ElforConfiguration.loading() => ElforConfiguration(false, null, null);
-  // factory ElforConfiguration.home() => ElforConfiguration(false, true, null);
-  // factory ElforConfiguration.project(Project project) => ElforConfiguration(false, true, project);
-  // factory ElforConfiguration.unknown() => ElforConfiguration(true, null, null);
+  factory ElforConfiguration.loading() => ElforConfiguration(false, false, null);
+  factory ElforConfiguration.home() => ElforConfiguration(false, true, null);
+  factory ElforConfiguration.project(Project project) => ElforConfiguration(false, true, project);
+  factory ElforConfiguration.unknown() => ElforConfiguration(true, true, null);
 
-  // bool get isUnknown => _show404;
-  // bool get isLoading => !_show404 && (_loggedIn == null || !_loggedIn!);
-  // bool get isHome => !_show404 && _loggedIn == true;
-  // bool get isProjectPage => !_show404 && _loggedIn == true && _selectedProject != null;
+  bool get isUnknown => _show404;
+  bool get isLoading => !_show404 && !_initiated;
+  bool get isHome => !_show404 && _initiated;
+  bool get isProjectPage => !_show404 && _initiated && _selectedProject != null;
 }
