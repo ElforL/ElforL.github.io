458b30db50283c63b45ef3f1b8ac0fda838b156b
Laith Shono <elfordev@gmail.com>
2022-06-26 21:01:39 +0300

    📝 Made router config project attribute a String?

--- lib/router/elfor_configuration.dart
+++ lib/router/elfor_configuration.dart
@@ -1,23 +1,23 @@
 import 'package:laith_shono/models/Project.dart';
 
 class ElforConfiguration {
-  final bool _show404;
-  final bool _initiated;
-  final Project? _selectedProject;
+  final bool show404;
+  final bool initiated;
+  final String? selectedProjectTitle;
 
   ElforConfiguration(
-    this._show404,
-    this._initiated,
-    this._selectedProject,
+    this.show404,
+    this.initiated,
+    this.selectedProjectTitle,
   );
 
   factory ElforConfiguration.loading() => ElforConfiguration(false, false, null);
   factory ElforConfiguration.home() => ElforConfiguration(false, true, null);
-  factory ElforConfiguration.project(Project project) => ElforConfiguration(false, true, project);
+  factory ElforConfiguration.project(String projectTitle) => ElforConfiguration(false, true, projectTitle);
   factory ElforConfiguration.unknown() => ElforConfiguration(true, true, null);
 
-  bool get isUnknown => _show404;
-  bool get isLoading => !_show404 && !_initiated;
-  bool get isHome => !_show404 && _initiated;
-  bool get isProjectPage => !_show404 && _initiated && _selectedProject != null;
+  bool get isUnknown => show404;
+  bool get isLoading => !show404 && !initiated;
+  bool get isHome => !show404 && initiated && selectedProjectTitle == null;
+  bool get isProjectPage => !show404 && initiated && selectedProjectTitle != null;
 }
