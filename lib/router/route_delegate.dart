ElforL/ElforL.github.io

commit 2e7e52035298e63f14e8f013f2e1f1875ab1e8d4

🐛 Add _selectedProjectTitle to fix 404 project on startup

lib/router/route_delegate.dart
@@ -23,12 +23,32 @@ class ElforRouterDelegate extends RouterDelegate<ElforConfiguration>
 
   bool _show404 = false;
   bool _initiated = false;
+
+  /// This one is used for projects before db init
+  ///
+  /// if the app was on startup with a url to a project
+  /// [setNewRoutePath] would try to set [_selectedProject] but can't because
+  /// the projects aren't loaded yet, so it'll set it to null and if the project the user is looking for doesn't exist
+  /// then it's a 404.
+  ///
+  /// [_selectedProjectTitle] helps by keeping track of the title of the selected project.
+  /// in [_init] After [dbServices.load] is finished, it checks if [_selectedProjectTitle] is not null and looks for the project
+  String? _selectedProjectTitle;
   Project? _selectedProject;
 
   _init() async {
     await dbServices.load();
 
     initiated = dbServices.initiated;
+    if (_selectedProjectTitle != null) {
+      final project = getProjectOfTitle(_selectedProjectTitle!);
+      if (project != null) {
+        selectedProject = project;
+      } else {
+        show404 = true;
+        selectedProject = null;
+      }
+    }
   }
 
   set show404(bool value) {
@@ -53,7 +73,15 @@ class ElforRouterDelegate extends RouterDelegate<ElforConfiguration>
   }
 
   set selectedProject(Project? value) {
+    if (dbServices.initiated) {
+      if (value != null) {
+        show404 = !dbServices.projects.contains(value);
+      } else {
+        show404 = true;
+      }
+    }
     _selectedProject = value;
+    _selectedProjectTitle = value?.title;
     notifyListeners();
   }
 
@@ -127,10 +155,9 @@ class ElforRouterDelegate extends RouterDelegate<ElforConfiguration>
       show404 = false;
       selectedProject = null;
     } else if (configuration.isProjectPage) {
-      final tempProject = getProjectOfTitle(configuration.selectedProjectTitle!);
-
-      show404 = tempProject == null;
-      selectedProject = tempProject;
+      show404 = false;
+      selectedProject = getProjectOfTitle(configuration.selectedProjectTitle!);
+      _selectedProjectTitle = configuration.selectedProjectTitle;
     } else {
       debugPrint("Couldn't set a new route for config $configuration");
     }

commit f936cf93a999eced9fa255c2a76faf5afd8def8a

🐛 Fix 404 on home page caused by 2e7e520

lib/router/route_delegate.dart
@@ -47,12 +47,9 @@ class ElforRouterDelegate extends RouterDelegate<ElforConfiguration>
       /// Read the documentation of [_selectedProjectTitle] to understand why this is here
       if (_selectedProjectTitle != null) {
         final project = getProjectOfTitle(_selectedProjectTitle!);
-        if (project != null) {
-          selectedProject = project;
-        } else {
-          show404 = true;
-          selectedProject = null;
-        }
+
+        show404 = project == null;
+        selectedProject = project;
       }
     } catch (e) {
       _show502 = true;
@@ -82,13 +79,10 @@ class ElforRouterDelegate extends RouterDelegate<ElforConfiguration>
   }
 
   set selectedProject(Project? value) {
-    if (dbServices.initiated) {
-      if (value != null) {
-        show404 = !dbServices.projects.contains(value);
-      } else {
-        show404 = true;
-      }
+    if (value != null && dbServices.initiated) {
+      show404 = !dbServices.projects.contains(value);
     }
+
     _selectedProject = value;
     _selectedProjectTitle = value?.title;
     notifyListeners();
@@ -170,9 +164,14 @@ class ElforRouterDelegate extends RouterDelegate<ElforConfiguration>
       show404 = false;
       selectedProject = null;
     } else if (configuration.isProjectPage) {
-      show404 = false;
-      selectedProject = getProjectOfTitle(configuration.selectedProjectTitle!);
-      _selectedProjectTitle = configuration.selectedProjectTitle;
+      var project = getProjectOfTitle(configuration.selectedProjectTitle!);
+      if (dbServices.initiated) {
+        selectedProject = project;
+        show404 = project == null;
+      } else {
+        show404 = false;
+        _selectedProjectTitle = configuration.selectedProjectTitle;
+      }
     } else {
       debugPrint("Couldn't set a new route for config $configuration");
     }